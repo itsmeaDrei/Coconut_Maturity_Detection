6fe305f1a1e5ef9439431b7d19f34333cfe22f69
vkilzold <princemaynardmasilac@gmail.com>
2025-05-05 14:05:10 +0800

    final design

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 UPLOAD_FOLDER = "static/uploads"
 PROCESSED_FOLDER = "static/processed"
-MODEL_PATH = "yolov5.pt"
+MODEL_PATH = "yoloversions/yolov5.pt"
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(PROCESSED_FOLDER, exist_ok=True)
